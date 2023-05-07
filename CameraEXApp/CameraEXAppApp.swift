b145950af0c7f4bb897cba25e23412f35f54af7f
fuziki <hymne.shannon.projects@gmail.com>
2023-05-07 23:16:19 +0900

    create CameraEXAppApp

---
+++ CameraEXApp/CameraEXAppApp.swift
@@ -0,0 +1,18 @@
+//
+//  CameraEXAppApp.swift
+//  CameraEXApp
+//  
+//  Created by fuziki on 2023/05/07
+//  
+//
+
+import SwiftUI
+
+@main
+struct CameraEXAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
