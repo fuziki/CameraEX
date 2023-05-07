8cfd60d30abcbed8fac35538d7c964ac206ca71a
fuziki <hymne.shannon.projects@gmail.com>
2023-05-07 23:18:44 +0900

    create GameApp

---
+++ GameApp/AppDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.swift
+//  GameApp
+//  
+//  Created by fuziki on 2023/05/07
+//  
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+}
